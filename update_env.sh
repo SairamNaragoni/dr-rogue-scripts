080a525ec100dfe56ca2201efc476e4e71fd14a2
SairamNaragoni <naragonisairam@gmail.com>
2023-06-13 13:24:59 -0400

    updating scripts

--- update_env.sh
+++ update_env.sh
@@ -9,10 +9,10 @@ cf_dir="${2:-custom_files}"
 
 aws s3 sync $BASE_S3_URI/$cf_dir/* $cf_dir
 
-public_dns_name=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${instance_name}" --query 'Reservations[*].Instances[*].[PublicDnsName]' --output text --max-items 1)
+public_dns_name=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${instance_name}" --query 'Reservations[*].Instances[*].[PublicDnsName]' --output text | tr -d '\n')
 
-scp -i $KEY_PAIR update_images.sh ubuntu@"$public_dns_name":/home/ubuntu/update_images.sh
-scp -i $KEY_PAIR $cf_dir/run.env ubuntu@"$public_dns_name":/home/ubuntu/deepracer-for-cloud/
-scp -i $KEY_PAIR $cf_dir/system.env ubuntu@"$public_dns_name":/home/ubuntu/deepracer-for-cloud/
+scp -i $KEY_PAIR.pem update_images.sh ubuntu@"$public_dns_name":/home/ubuntu/update_images.sh
+scp -i $KEY_PAIR.pem $cf_dir/run.env ubuntu@"$public_dns_name":/home/ubuntu/deepracer-for-cloud/
+scp -i $KEY_PAIR.pem $cf_dir/system.env ubuntu@"$public_dns_name":/home/ubuntu/deepracer-for-cloud/
 
 ./ssh_into_rogue.sh $instance_name
