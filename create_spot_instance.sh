bea35316dca9eaa5acaaf75cb3030adf10eb2515
SairamNaragoni <naragonisairam@gmail.com>
2023-06-12 23:43:25 -0400

    updating scripts

--- create_spot_instance.sh
+++ create_spot_instance.sh
@@ -2,12 +2,12 @@
 
 source config.conf
 
-instance_name=${1:-rogue}
-instance_type=${1:-"g4dn.4xlarge"}
+instance_name=${1:-"rogue"}
+instance_type=${2:-"g4dn.4xlarge"}
 
 aws ec2 run-instances --image-id $IMAGE_ID --count 1 --instance-type $instance_type \
 --key-name $KEY_PAIR --security-group-ids $SECURITY_GROUP_ID --subnet-id $SUBNET_ID \
---tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=${instance_name}]'  \
+--tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance_name]'  \
 --iam-instance-profile Name=$IAM_ROLE \
 --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=one-time,InstanceInterruptionBehavior=terminate}"
 
