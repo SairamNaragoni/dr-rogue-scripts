57a700942fd1e49f444384970e30a840054172c3
SairamNaragoni <naragonisairam@gmail.com>
2023-06-13 00:08:42 -0400

    updating scripts

--- ssh_into_rogue.sh
+++ ssh_into_rogue.sh
@@ -4,6 +4,6 @@ source config.conf
 
 instance_name="${1:-rogue}"
 
-public_dns_name=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${instance_name}" --query 'Reservations[*].Instances[*].[PublicDnsName]' --output text --max-items 1)
+public_dns_name=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${instance_name}" --query 'Reservations[*].Instances[*].[PublicDnsName]' --output text | tr -d '\n')
 echo "SSHing into: "$public_dns_name
-ssh -i $KEY_PAIR ubuntu@"$public_dns_name"
+ssh -i $KEY_PAIR.pem ubuntu@"$public_dns_name"
