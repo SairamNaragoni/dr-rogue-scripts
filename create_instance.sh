aff8473e270479d4728b09158a98d5b480333c82
SairamNaragoni <naragonisairam@gmail.com>
2023-06-12 23:47:02 -0400

    updating scripts

--- create_instance.sh
+++ create_instance.sh
@@ -7,7 +7,7 @@ instance_type=${1:-"g4dn.4xlarge"}
 
 aws ec2 run-instances --image-id $IMAGE_ID --count 1 --instance-type $instance_type \
 --key-name $KEY_PAIR --security-group-ids $SECURITY_GROUP_ID --subnet-id $SUBNET_ID \
---tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance_name}]'  \
+--tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=\"$instance_name\"}]'  \
 --iam-instance-profile Name=$IAM_ROLE \
 
 ./update_sec_group.sh
